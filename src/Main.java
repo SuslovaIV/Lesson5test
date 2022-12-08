1f5191e45c78946bef6441aedecb5e6e1ec4aea1
TeenGale <isis86@yandex.ru>
2022-12-08 23:04:40 +0300

    + thread

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,35 @@ public class Main {
        MyPrint_ task = new MyPrint_();
        Print print = new Print(task);
        new Thread(print).start();
-       new Thread(print).start();
+       /*new Thread(print).start();
+       new Thread(()-> {
+           try {
+               task.action();
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       }).start();
+       new Thread(()-> {
+           try {
+               task.up();
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       }).start();*/
     }
 }
 class MyPrint_ {
     private int j =1;
-    public synchronized void action() {
+    public synchronized void action() throws InterruptedException {
         while (j<=5) {
             System.out.print("ABC");
-            //wait();
-            j++;
+            wait();
         }
+        j++;
         notify();
+    }
+    public synchronized void up() throws InterruptedException {
+        j++;
     }
 }
 class Print implements Runnable{
@@ -23,8 +40,13 @@ class Print implements Runnable{
         this.print = print;
     }
     public void run(){
-        for (int i = 1; i < 6; i++) {
-            print.action();
+        for (int j = 1; j < 6; j++) {
+            try {
+                print.action();
+                j++;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
---
+++ src/Thread/MyThread_Task1.java
@@ -0,0 +1,63 @@
+package Thread;
+
+public class MyThread_Task1 {
+    public static void main(String[] args) {
+        TestThread thread=new TestThread();
+        Thread1 thread1 = new Thread1(thread);
+        Thread2 thread2 = new Thread2(thread);
+        new Thread(thread1).start();
+        new Thread(thread2).start();
+    }
+}
+class TestThread{
+    private boolean running1 = false;
+    private boolean running2 = false;
+    public synchronized void action1() {
+        while (running1) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+        // running1 = false; не понимаю, почему работает. переменнеы running1 running2 никогда не становятся true
+        // running2 = true;
+        System.out.print("ABC");
+        notify();
+    }
+    public synchronized void action2() {
+        while (running2) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+        // running2 = true;
+        // running1 = false;
+        System.out.print("ABC");
+        notify();
+    }
+}
+class Thread1 implements Runnable{
+    TestThread thread;
+    Thread1(TestThread thread){
+        this.thread=thread;
+    }
+    public void run(){
+        for (int i = 1; i < 6; i++) {
+            thread.action1();
+        }
+    }
+}
+class Thread2 implements Runnable{
+    TestThread thread;
+    Thread2(TestThread thread){
+        this.thread=thread;
+    }
+    public void run(){
+        for (int i = 1; i < 6; i++) {
+            thread.action2();
+        }
+    }
+}
